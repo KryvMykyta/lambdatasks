0c929a8981cc06881e0490866dd04f4d65979065
Kryv Mykyta <kryvmykyta@gmail.com>
2023-02-28 07:07:57 +0200

    rework custom methods

--- 19_custom_methods/app.ts
+++ 19_custom_methods/app.ts
@@ -1,27 +1,27 @@
 interface Array<T> {
   multiply(this: Array<number>, multiplier?: number): Array<number>;
-  all(func: (v: T) => boolean): boolean;
-  any(func: (v: T) => boolean): boolean;
+  all(this: Array<T>, func: (v: T) => boolean): boolean;
+  any(this: Array<T>, func: (v: T) => boolean): boolean;
   associateBy(this: { [key: string]: any }[], key: string, value?: string): Array<Object>;
   average(this: Array<number>): number;
-  chunked(size: number): Array<Array<T>>;
-  distinctBy(func?: Function): Array<Object>;
-  filter1(func: (v: T) => boolean): Array<T>;
-  filterIndexed(func: (v: T, u: T) => boolean): Array<T>;
-  filterNot(func: (v: T) => boolean): Array<T>;
-  find1(func: (v: T) => boolean): T;
-  findLast(func: (v: T) => boolean): T;
-  flatten(count?: number): Array<T>;
-  fold(initial: any, func: Function): any;
-  maxBy(func: Function): T;
-  minBy(func: Function): T;
-  count(key?: string): number;
-  groupByKey(func: Function): { [key: string]: T };
+  chunked(this: Array<T>, size: number): Array<Array<T>>;
+  distinctBy(this: Array<T>, func?: Function): Array<Object>;
+  filter1(this: Array<T>, func: (v: T) => boolean): Array<T>;
+  filterIndexed(this: Array<T>, func: (v: T, u: T) => boolean): Array<T>;
+  filterNot(this: Array<T>, func: (v: T) => boolean): Array<T>;
+  find1(this: Array<T>, func: (v: T) => boolean): T;
+  findLast(this: Array<T>, func: (v: T) => boolean): T;
+  flatten(this: Array<T>, count?: number): Array<T>;
+  fold(this: Array<T>, initial: any, func: Function): any;
+  maxBy(this: Array<T>, func: Function): T;
+  minBy(this: Array<T>, func: Function): T;
+  count(this: Array<T>, key?: string): number;
+  groupByKey(this: Array<T>, func: Function): { [key: string]: T[] };
 }
 
 console.log("multiply");
 Array.prototype.multiply = function (multiplier = 10) {
-  let resArray: Array<number> = [];
+  const resArray: Array<number> = [];
   this.forEach((element) => {
     resArray.push(element * multiplier);
   });
@@ -67,7 +67,7 @@ console.log(
 
 console.log("associateBy");
 Array.prototype.associateBy = function (key: string, value: string) {
-  let resultArray: Array<Object> = [];
+  const resultArray: Array<Object> = [];
   if (!value) {
     this.forEach((element) => {
       const newKey = element[key];
@@ -99,7 +99,7 @@ console.log(array.associateBy("emoji", "sad"));
 
 console.log("chunked");
 Array.prototype.chunked = function <T>(size: number) {
-  let resultArray: Array<T[]> = [];
+  const resultArray: Array<T[]> = [];
   let stepArray: Array<T> = [];
   this.forEach((element: T) => {
     if (stepArray.length === size) {
@@ -120,7 +120,7 @@ Array.prototype.distinctBy = function <T>(func?: Function) {
   if (!func) {
     return Array.from(new Set(this));
   } else {
-    let resultArray: Array<T> = [];
+    const resultArray: Array<T> = [];
     this.forEach((element: T) => resultArray.push(func(element)));
     return Array.from(new Set(resultArray));
   }
@@ -132,7 +132,7 @@ console.log(arr.distinctBy((el: string) => el.toLowerCase()));
 
 console.log("filter");
 Array.prototype.filter1 = function <T>(func: (v: T) => boolean) {
-  let resultArray: Array<T> = [];
+  const resultArray: Array<T> = [];
   this.forEach((element: T) => {
     if (func(element)) resultArray.push(element);
   });
@@ -148,7 +148,7 @@ console.log(
 
 console.log("filterIndexed");
 Array.prototype.filterIndexed = function <T>(func: (v: number, u: T) => boolean) {
-  let resultArray: Array<T> = [];
+  const resultArray: Array<T> = [];
   for (let index = 0; index < this.length; index++) {
     if (func(index, this[index])) {
       resultArray.push(this[index]);
@@ -166,7 +166,7 @@ console.log(
 
 console.log("filterNot");
 Array.prototype.filterNot = function <T>(func: (v: T) => boolean) {
-  let resultArr: Array<T> = [];
+  const resultArr: Array<T> = [];
   this.map((element: T) => {
     if (!func(element)) resultArr.push(element);
   });
@@ -182,7 +182,7 @@ console.log(
 console.log("find");
 Array.prototype.find1 = function <T>(func: (v: T) => boolean) {
   for (let index = 0; index < this.length; index++) {
-    if (func(this[index])) return this[index];
+    if (func(this[index])) return index;
   }
   return null;
 };
@@ -197,7 +197,7 @@ console.log(
 console.log("findLast");
 Array.prototype.findLast = function (func: (v: number) => boolean) {
   const reversed = this.reverse();
-  return reversed.find1(func);
+  return this.length - reversed.find1(func) - 1;
 };
 
 console.log(
@@ -206,121 +206,122 @@ console.log(
   })
 );
 
-// console.log("flatten");
-// Array.prototype.flatten = function <T>(count = 0) {
-//   const isIterable = (object: any) =>
-//     object != null && typeof object[Symbol.iterator] === "function";
-//   let resArr = this;
-//   if (count !== 0) {
-//     for (let i = 0; i < count; i++) {
-//       let stepArr: Array<T> = [];
-//       for (let element of resArr) {
-//         if (isIterable(element)) {
-//           for (let newEl of element) {
-//             stepArr.push(newEl);
-//           }
-//         } else {
-//           stepArr.push(element);
-//         }
-//       }
-//       resArr = stepArr;
-//     }
-//   } else {
-//     while (resArr.filter((el: T) => isIterable(el)).length !== 0) {
-//       let stepArr: Array<T> = [];
-//       for (let element of resArr) {
-//         if (isIterable(element)) {
-//           for (let newEl of element) {
-//             stepArr.push(newEl);
-//           }
-//         } else {
-//           stepArr.push(element);
-//         }
-//       }
-//       resArr = stepArr;
-//     }
-//   }
-//   return resArr;
-// };
+console.log("flatten");
+Array.prototype.flatten = function (count = 0) {
+  const isIterable = (object: any) =>
+    object != null && typeof object[Symbol.iterator] === "function";
+  let resArr : Array<any> = this;
+  if (count !== 0) {
+    for (let i = 0; i < count; i++) {
+      const stepArr : Array<any> = [];
+      resArr.forEach(element => {
+        if(isIterable(element)) {
+          element.forEach((newElement: any) => {
+            stepArr.push(newElement)
+          });
+        }else {
+          stepArr.push(element)
+        }
+      });
+      resArr = stepArr;
+    }
+  } else {
+    while (resArr.filter((el: any) => isIterable(el)).length !== 0) {
+      const stepArr : Array<any> = [];
+      resArr.forEach(element => {
+        if(isIterable(element)) {
+          element.forEach((newElement: any) => {
+            stepArr.push(newElement)
+          });
+        }else {
+          stepArr.push(element)
+        }
+      });
+      resArr = stepArr;
+    }
+  }
+  return resArr;
+};
 
-// console.log([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]].flatten(1));
+console.log([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]].flatten(1));
 
-// console.log("fold");
-// Array.prototype.fold = function (initial: any, func: Function) {
-//   for (let el of this) {
-//     initial = func(initial, el);
-//   }
-//   return initial;
-// };
+console.log("fold");
+Array.prototype.fold = function (initial: any, func: Function) {
+  this.forEach((element: any) => {
+    initial = func(initial, element);
+  });
+  return initial;
+};
 
-// console.log(
-//   [1, 2, 3, 4, 5].fold(
-//     1,
-//     (initial: number, element: number) => initial * element
-//   )
-// );
+console.log(
+  [1, 2, 3, 4, 5].fold(
+    1,
+    (initial: number, element: number) => initial * element
+  )
+);
 
-// Array.prototype.maxBy = function (func: Function) {
-//   let maxEl = this[0];
-//   for (let el of this) {
-//     if (func(el) > func(maxEl)) maxEl = el;
-//   }
-//   return maxEl;
-// };
+Array.prototype.maxBy = function (func: Function) {
+  let maxEl = this[0];
+  this.forEach((element: any) => {
+    if (func(element) > func(maxEl)) maxEl = element;
+  });
+  return maxEl;
+};
 
-// Array.prototype.minBy = function (func: Function) {
-//   let minEl = this[0];
-//   for (let el of this) {
-//     if (func(el) < func(minEl)) minEl = el;
-//   }
-//   return minEl;
-// };
+Array.prototype.minBy = function (func: Function) {
+  let minEl = this[0];
+  this.forEach((element: any) => {
+    if (func(element) < func(minEl)) minEl = element;
+  });
+  return minEl;
+};
 
-// Array.prototype.count = function (key?: string): number {
-//   if (!key) {
-//     return this.length;
-//   } else {
-//     let sum = 0;
-//     for (let el of this) {
-//       if (typeof el === typeof Object) {
-//         if (Object.keys(el).includes(key)) {
-//           if (!isNaN(Number(el[key]))) sum += Number(el[key]);
-//         }
-//       }
-//     }
-//     return sum;
-//   }
-// };
+Array.prototype.count = function (key?: string): number {
+  if (!key) {
+    return this.length;
+  } else {
+    let sum = 0;
+    this.forEach(element => {
+      if (typeof element === typeof Object) {
+        if (Object.keys(element).includes(key)) {
+          if (!isNaN(Number(element[key]))) sum += Number(element[key]);
+        }
+      }
+    })
+    return sum;
+  }
+};
 
-// Array.prototype.groupByKey = function (func: Function) {
-//   let resObj: { [key: string]: any } = {};
-//   for (let el of this) {
-//     let res = func(el);
-//     if (!Object.keys(resObj).includes(String(res))) {
-//       resObj[res] = [el];
-//     } else {
-//       resObj[res].push(el);
-//     }
-//   }
-//   return resObj;
-// };
+Array.prototype.groupByKey = function<T>(func: Function) {
+  let resultObject: { [key: string]: T[] } = {};
 
-// let groupbytest = ["a", "abc", "ab", "def", "abcd"];
-// console.log(groupbytest.groupByKey((el: any) => el.length));
+  this.forEach((element: T) => {
+    const result = func(element);
+    if (!Object.keys(resultObject).includes(String(result))) {
+      resultObject[result] = [element];
+    } else {
+      resultObject[result].push(element);
+    }
+  })
+  return resultObject;
+};
 
-// let object = [
-//   {
-//     name: "Alice",
-//     Work: "Marketing",
-//   },
-//   {
-//     name: "Bob",
-//     Work: "Sales",
-//   },
-//   {
-//     name: "Carol",
-//     Work: "Marketing",
-//   },
-// ];
+let groupbytest = ["a", "abc", "ab", "def", "abcd"];
+console.log(groupbytest.groupByKey((el: any) => el.length));
 
-// console.log(object.groupByKey((el: any) => el.Work));
+let object = [
+  {
+    name: "Alice",
+    Work: "Marketing",
+  },
+  {
+    name: "Bob",
+    Work: "Sales",
+  },
+  {
+    name: "Carol",
+    Work: "Marketing",
+  },
+];
+
+console.log(object.groupByKey((el: any) => el.Work));
